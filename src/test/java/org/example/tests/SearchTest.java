a0d83685c5d1fd29780dad75f2e08bfe39b7ebd7
damiankadziela81 <damiankadziela02@gmail.com>
2025-02-17 10:41:03 +0100

    Add parametrized test

--- src/test/java/org/example/tests/SearchTest.java
+++ src/test/java/org/example/tests/SearchTest.java
@@ -4,9 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.example.pages.HomePage;
 import org.example.pages.SearchResultPage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 class SearchTest extends BaseTest{
 
@@ -36,10 +41,26 @@ class SearchTest extends BaseTest{
         Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isEqualTo(4);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"mug", "t-shirt", "frame"})
-    void shouldReturnProductsByProductName(String productName) {
+//    @ParameterizedTest
+//    @ValueSource(strings = {"mug", "t-shirt", "frame"})
+//    void shouldReturnProductsByProductName(String productName) {
+//        SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts(productName);
+//        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isGreaterThan(0);
+//    }
+
+    @DisplayName("Search for products")
+    @ParameterizedTest(name = "Search for {0} should return {1} product(s)")
+    @MethodSource("searchData")
+    void shouldReturnProductsByProductName(String productName, int productCount) {
         SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts(productName);
-        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isGreaterThan(0);
+        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isEqualTo(productCount);
+    }
+
+    private static Stream<Arguments> searchData() {
+        return Stream.of(
+                Arguments.of("mug", 5),
+                Arguments.of("t-shirt", 1),
+                Arguments.of("frame", 4)
+        );
     }
 }

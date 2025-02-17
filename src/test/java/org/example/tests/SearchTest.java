7c62a2575ce3f375cd0b244c068c731a45f9dfb3
damiankadziela81 <damiankadziela02@gmail.com>
2025-02-17 10:27:51 +0100

    Add more tests

--- src/test/java/org/example/tests/SearchTest.java
+++ src/test/java/org/example/tests/SearchTest.java
@@ -5,6 +5,8 @@ import org.example.pages.HomePage;
 import org.example.pages.SearchResultPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SearchTest extends BaseTest{
 
@@ -13,12 +15,31 @@ class SearchTest extends BaseTest{
     @BeforeEach
     void beforeEach() {
         homePage = new HomePage(page);
+        page.navigate("https://skleptestera.pl");
     }
 
     @Test
     void shouldReturnProductsBySearchNameMug() {
-        page.navigate("https://skleptestera.pl");
         SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts("mug");
         Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldReturnProductsBySearchNameTshirt() {
+        SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts("t-shirt");
+        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReturnProductsBySearchNameFrame() {
+        SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts("frame");
+        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"mug", "t-shirt", "frame"})
+    void shouldReturnProductsByProductName(String productName) {
+        SearchResultPage searchResultPage = homePage.getTopMenuAndSearchSection().searchForProducts(productName);
+        Assertions.assertThat(searchResultPage.getSearchResultsSection().getProducts().size()).isGreaterThan(0);
     }
 }

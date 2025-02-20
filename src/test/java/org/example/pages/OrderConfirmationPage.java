417e917908f53383f786b273e8d6a9465536b1b5
damiankadziela81 <damiankadziela02@gmail.com>
2025-02-20 10:48:52 +0100

    Added junit-platform.properties to run tests in parallel

--- src/test/java/org/example/pages/OrderConfirmationPage.java
+++ src/test/java/org/example/pages/OrderConfirmationPage.java
@@ -3,6 +3,7 @@ package org.example.pages;
 import com.microsoft.playwright.Page;
 import lombok.Getter;
 import org.example.pages.sections.orderConfirmationPage.OrderConfirmationDetailsSection;
+import org.example.utils.PageUtils;
 
 @Getter
 public class OrderConfirmationPage extends BasePage{
@@ -10,6 +11,7 @@ public class OrderConfirmationPage extends BasePage{
     private OrderConfirmationDetailsSection orderConfirmationDetailsSection;
     public OrderConfirmationPage(Page page) {
         super(page);
+        PageUtils.waitForPageToLoad(page);
         this.orderConfirmationDetailsSection = new OrderConfirmationDetailsSection(page);
     }
 }
